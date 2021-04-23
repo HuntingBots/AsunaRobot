6606ed912c3547aaa04c7b565ce4f16aae84f733
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-04-23 11:22:37 +0530

    Update dbcleanup.py

--- AsunaRobot/modules/dbcleanup.py
+++ AsunaRobot/modules/dbcleanup.py
@@ -1,9 +1,9 @@
 from time import sleep
 
-import YoneRobot.modules.sql.global_bans_sql as gban_sql
-import YoneRobot.modules.sql.users_sql as user_sql
-from YoneRobot import DEV_USERS, OWNER_ID, dispatcher
-from YoneRobot.modules.helper_funcs.chat_status import dev_plus
+import AsunaRobot.modules.sql.global_bans_sql as gban_sql
+import AsunaRobot.modules.sql.users_sql as user_sql
+from AsunaRobot import DEV_USERS, OWNER_ID, dispatcher
+from AsunaRobot.modules.helper_funcs.chat_status import dev_plus
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.error import BadRequest, Unauthorized
 from telegram.ext import (

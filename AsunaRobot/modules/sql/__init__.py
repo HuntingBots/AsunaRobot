9c53ccca0e2ad0ec608b4d74cb1ab32ecb6adb19
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-04-23 15:50:32 +0530

    Update __init__.py

--- AsunaRobot/modules/sql/__init__.py
+++ AsunaRobot/modules/sql/__init__.py
@@ -1,4 +1,4 @@
-from YoneRobot import DB_URI
+from AsunaRobot import DB_URI
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker

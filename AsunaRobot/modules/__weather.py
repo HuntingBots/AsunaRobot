83e4b7340243909a62f18b55dbde5e87ad5018ca
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-04-23 10:14:56 +0530

    Update __weather.py

--- AsunaRobot/modules/__weather.py
+++ AsunaRobot/modules/__weather.py
@@ -1,4 +1,4 @@
-from YoneRobot import telethn as tbot
+from AsunaRobot import telethn as tbot
 import io
 import os
 import time
@@ -9,9 +9,9 @@ from telethon.tl import functions
 from telethon.tl import types
 from telethon.tl.types import *
 
-from YoneRobot import *
+from AsunaRobot import *
 
-from YoneRobot.events import register
+from AsunaRobot.events import register
 
 
 async def is_register_admin(chat, user):

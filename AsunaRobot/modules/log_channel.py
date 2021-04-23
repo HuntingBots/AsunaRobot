139478241f3374aabaf6f00d2a398bf3e3106a6d
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-04-23 11:55:37 +0530

    Update log_channel.py

--- AsunaRobot/modules/log_channel.py
+++ AsunaRobot/modules/log_channel.py
@@ -3,7 +3,7 @@ from functools import wraps
 
 from telegram.ext import CallbackContext
 
-from YoneRobot.modules.helper_funcs.misc import is_module_loaded
+from AsunaRobot.modules.helper_funcs.misc import is_module_loaded
 
 FILENAME = __name__.rsplit(".", 1)[-1]
 
@@ -13,9 +13,9 @@ if is_module_loaded(FILENAME):
     from telegram.ext import CommandHandler, JobQueue, run_async
     from telegram.utils.helpers import escape_markdown
 
-    from YoneRobot import EVENT_LOGS, LOGGER, dispatcher
-    from YoneRobot.modules.helper_funcs.chat_status import user_admin
-    from YoneRobot.modules.sql import log_channel_sql as sql
+    from AsunaRobot import EVENT_LOGS, LOGGER, dispatcher
+    from AsunaRobot.modules.helper_funcs.chat_status import user_admin
+    from AsunaRobot.modules.sql import log_channel_sql as sql
 
     def loggable(func):
         @wraps(func)

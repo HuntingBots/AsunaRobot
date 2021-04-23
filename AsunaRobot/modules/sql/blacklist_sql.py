90de404dc48a7f14c4cc95cc7b3f043f344eb7fb
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-04-23 15:57:50 +0530

    Update blacklist_sql.py

--- AsunaRobot/modules/sql/blacklist_sql.py
+++ AsunaRobot/modules/sql/blacklist_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import func, distinct, Column, String, UnicodeText, Integer
 
-from YoneRobot.modules.sql import SESSION, BASE
+from AsunaRobot.modules.sql import SESSION, BASE
 
 
 class BlackListFilters(BASE):

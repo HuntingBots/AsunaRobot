3e89b03a21aeecd785c2b7526de6d8b4999c1b05
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-04-23 12:42:03 +0530

    Update shell.py

--- AsunaRobot/modules/shell.py
+++ AsunaRobot/modules/shell.py
@@ -1,7 +1,7 @@
 import subprocess
 
-from YoneRobot import LOGGER, dispatcher
-from YoneRobot.modules.helper_funcs.chat_status import dev_plus
+from AsunaRobot import LOGGER, dispatcher
+from AsunaRobot.modules.helper_funcs.chat_status import dev_plus
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, CommandHandler
 from telegram.ext.dispatcher import run_async

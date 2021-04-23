6b34fb3bb0f4bec7e762ce58dc10aac83da3a2ba
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-04-23 12:41:15 +0530

    Update sed.py

--- AsunaRobot/modules/sed.py
+++ AsunaRobot/modules/sed.py
@@ -2,9 +2,9 @@ import sre_constants
 
 import regex
 import telegram
-from YoneRobot import LOGGER, dispatcher
-from YoneRobot.modules.disable import DisableAbleMessageHandler
-from YoneRobot.modules.helper_funcs.regex_helper import infinite_loop_check
+from AsunaRobot import LOGGER, dispatcher
+from AsunaRobot.modules.disable import DisableAbleMessageHandler
+from AsunaRobot.modules.helper_funcs.regex_helper import infinite_loop_check
 from telegram import Update
 from telegram.ext import CallbackContext, Filters, run_async
 

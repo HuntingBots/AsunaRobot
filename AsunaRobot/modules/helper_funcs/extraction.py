9370bf423845963426c4887153ab72292481bae6
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-04-23 16:26:58 +0530

    Update extraction.py

--- AsunaRobot/modules/helper_funcs/extraction.py
+++ AsunaRobot/modules/helper_funcs/extraction.py
@@ -1,7 +1,7 @@
 from typing import List, Optional
 
-from YoneRobot import LOGGER
-from YoneRobot.modules.users import get_user_id
+from AsunaRobot import LOGGER
+from AsunaRobot.modules.users import get_user_id
 from telegram import Message, MessageEntity
 from telegram.error import BadRequest
 

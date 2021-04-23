2c3b6e5059b8da005a25f78fb6de6e36b1bb5c6b
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-04-23 11:02:59 +0530

    Update currency_converter.py

--- AsunaRobot/modules/currency_converter.py
+++ AsunaRobot/modules/currency_converter.py
@@ -1,5 +1,5 @@
 import requests
-from YoneRobot import CASH_API_KEY, dispatcher
+from AsunaRobot import CASH_API_KEY, dispatcher
 from telegram import Update, ParseMode
 from telegram.ext import CallbackContext, CommandHandler, run_async
 

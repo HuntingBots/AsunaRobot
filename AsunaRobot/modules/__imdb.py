fe7984120c36a407664750ea339cca8e2e8aedb4
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-04-23 10:01:06 +0530

    Update __imdb.py

--- AsunaRobot/modules/__imdb.py
+++ AsunaRobot/modules/__imdb.py
@@ -1,11 +1,11 @@
-from YoneRobot import telethn as tbot
+from AsunaRobot import telethn as tbot
 import os
 import re
 import bs4
 import requests
 from telethon import types
 from telethon.tl import functions
-from YoneRobot.events import register
+from AsunaRobot.events import register
 
 langi = "en"
 

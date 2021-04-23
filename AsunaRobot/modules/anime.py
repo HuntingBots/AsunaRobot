8b05b203b515c18f471ea51065361f6741a1ff6c
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-04-23 10:27:54 +0530

    Update anime.py

--- AsunaRobot/modules/anime.py
+++ AsunaRobot/modules/anime.py
@@ -5,8 +5,8 @@ import textwrap
 import bs4
 import jikanpy
 import requests
-from YoneRobot import DEV_USERS, OWNER_ID, DRAGONS, dispatcher
-from YoneRobot.modules.disable import DisableAbleCommandHandler
+from AsunaRobot import DEV_USERS, OWNER_ID, DRAGONS, dispatcher
+from AsunaRobot.modules.disable import DisableAbleCommandHandler
 from telegram import (InlineKeyboardButton, InlineKeyboardMarkup, ParseMode,
                       Update)
 from telegram.ext import CallbackContext, CallbackQueryHandler, run_async

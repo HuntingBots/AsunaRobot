cf93c50bf8698af62ef81ab0c4021e3ecbaafca8
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-04-23 11:48:11 +0530

    Update gtranslator.py

--- AsunaRobot/modules/gtranslator.py
+++ AsunaRobot/modules/gtranslator.py
@@ -3,8 +3,8 @@ from google_trans_new import LANGUAGES, google_translator
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, run_async
 
-from YoneRobot import dispatcher
-from YoneRobot.modules.disable import DisableAbleCommandHandler
+from AsunaRobot import dispatcher
+from AsunaRobot.modules.disable import DisableAbleCommandHandler
 
 
 @run_async

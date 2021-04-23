569b11ccae657ab5535190c16c5b023a915c2b72
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-04-23 16:21:18 +0530

    Update night_mode_sql.py

--- AsunaRobot/modules/sql_extended/night_mode_sql.py
+++ AsunaRobot/modules/sql_extended/night_mode_sql.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Boolean, Column, Integer, String, UnicodeText
-from YoneRobot.modules.sql import BASE, SESSION
+from AsunaRobot.modules.sql import BASE, SESSION
 
 
 class Nightmode(BASE):

9748c545f1a4d016aca3d8febb5a9b4edee44c63
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-04-23 12:26:29 +0530

    Update muting.py

--- AsunaRobot/modules/muting.py
+++ AsunaRobot/modules/muting.py
@@ -1,20 +1,20 @@
 import html
 from typing import Optional
 
-from YoneRobot import LOGGER, TIGERS, dispatcher
-from YoneRobot.modules.helper_funcs.chat_status import (
+from AsunaRobot import LOGGER, TIGERS, dispatcher
+from AsunaRobot.modules.helper_funcs.chat_status import (
     bot_admin,
     can_restrict,
     connection_status,
     is_user_admin,
     user_admin,
 )
-from YoneRobot.modules.helper_funcs.extraction import (
+from AsunaRobot.modules.helper_funcs.extraction import (
     extract_user,
     extract_user_and_text,
 )
-from YoneRobot.modules.helper_funcs.string_handling import extract_time
-from YoneRobot.modules.log_channel import loggable
+from AsunaRobot.modules.helper_funcs.string_handling import extract_time
+from AsunaRobot.modules.log_channel import loggable
 from telegram import Bot, Chat, ChatPermissions, ParseMode, Update
 from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler, run_async

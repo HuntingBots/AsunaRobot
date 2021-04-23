efcd9e9b0dd86be7e15618d8d8fe01a19b4b4cab
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-04-23 09:52:18 +0530

    Update __forcesubs.py

--- AsunaRobot/modules/__forcesubs.py
+++ AsunaRobot/modules/__forcesubs.py
@@ -12,9 +12,9 @@ from pyrogram.errors.exceptions.bad_request_400 import (
 )
 from pyrogram.types import ChatPermissions, InlineKeyboardButton, InlineKeyboardMarkup
 
-from YoneRobot import DRAGONS as SUDO_USERS
-from YoneRobot import pbot
-from YoneRobot.modules.sql_extended import forceSubscribe_sql as sql
+from AsunaRobot import DRAGONS as SUDO_USERS
+from AsunaRobot import pbot
+from AsunaRobot.modules.sql_extended import forceSubscribe_sql as sql
 
 logging.basicConfig(level=logging.INFO)
 

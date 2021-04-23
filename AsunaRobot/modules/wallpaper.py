cf165bde59cf3a225f9922d82b1b9429462d325f
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-04-23 12:53:30 +0530

    Update wallpaper.py

--- AsunaRobot/modules/wallpaper.py
+++ AsunaRobot/modules/wallpaper.py
@@ -1,8 +1,8 @@
 from random import randint
 
 import requests as r
-from YoneRobot import SUPPORT_CHAT, WALL_API, dispatcher
-from YoneRobot.modules.disable import DisableAbleCommandHandler
+from AsunaRobot import SUPPORT_CHAT, WALL_API, dispatcher
+from AsunaRobot.modules.disable import DisableAbleCommandHandler
 from telegram import Update
 from telegram.ext import CallbackContext, run_async
 

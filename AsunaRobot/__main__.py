3351dac04fd6c3aa0f545aa83c9b7aa13047f9ec
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-04-23 18:07:59 +0530

    Update __main__.py

--- AsunaRobot/__main__.py
+++ AsunaRobot/__main__.py
@@ -118,7 +118,7 @@ CHAT_SETTINGS = {}
 USER_SETTINGS = {}
 
 for module_name in ALL_MODULES:
-    imported_module = importlib.import_module("YoneRobot.modules." + module_name)
+    imported_module = importlib.import_module("AsunaRobot.modules." + module_name)
     if not hasattr(imported_module, "__mod_name__"):
         imported_module.__mod_name__ = imported_module.__name__
 

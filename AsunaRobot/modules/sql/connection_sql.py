2205414bf41f8bcd19f5346ff4ff37a85d0d43cf
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-04-23 16:02:51 +0530

    Update connection_sql.py

--- AsunaRobot/modules/sql/connection_sql.py
+++ AsunaRobot/modules/sql/connection_sql.py
@@ -4,7 +4,7 @@ from typing import Union
 
 from sqlalchemy import Column, String, Boolean, UnicodeText, Integer
 
-from YoneRobot.modules.sql import SESSION, BASE
+from AsunaRobot.modules.sql import SESSION, BASE
 
 
 class ChatAccessConnectionSettings(BASE):

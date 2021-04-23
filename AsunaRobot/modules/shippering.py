a1f3ad4b8c463943374264794e215c8ccfe42d40
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-04-23 12:42:47 +0530

    Update shippering.py

--- AsunaRobot/modules/shippering.py
+++ AsunaRobot/modules/shippering.py
@@ -1,5 +1,5 @@
-from YoneRobot import pbot as app
-from YoneRobot.utils.dbfunc import get_couple, save_couple
+from AsunaRobot import pbot as app
+from AsunaRobot.utils.dbfunc import get_couple, save_couple
 from pyrogram import filters
 import random
 from datetime import datetime

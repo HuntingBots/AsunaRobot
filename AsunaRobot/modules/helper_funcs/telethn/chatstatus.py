58805ba1f52ca75ff4261489fc813636c9502b2a
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-04-23 16:33:12 +0530

    Update chatstatus.py

--- AsunaRobot/modules/helper_funcs/telethn/chatstatus.py
+++ AsunaRobot/modules/helper_funcs/telethn/chatstatus.py
@@ -1,5 +1,5 @@
-from YoneRobot.modules.helper_funcs.telethn import IMMUNE_USERS, telethn
-from YoneRobot import DRAGONS
+from AsunaRobot.modules.helper_funcs.telethn import IMMUNE_USERS, telethn
+from AsunaRobot import DRAGONS
 from telethon.tl.types import ChannelParticipantsAdmins
 
 

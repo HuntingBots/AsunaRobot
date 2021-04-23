81a20983a7127b2ef8798f946b690fbc9975fb16
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-04-23 15:53:42 +0530

    Update antiflood_sql.py

--- AsunaRobot/modules/sql/antiflood_sql.py
+++ AsunaRobot/modules/sql/antiflood_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import String, Column, Integer, UnicodeText
 
-from YoneRobot.modules.sql import SESSION, BASE
+from AsunaRobot.modules.sql import SESSION, BASE
 
 DEF_COUNT = 1
 DEF_LIMIT = 0

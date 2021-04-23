2aae9a32eb2dd7b967db3a4a3f2518a04d1d8c0b
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-04-23 15:56:59 +0530

    Update approve_sql.py

--- AsunaRobot/modules/sql/approve_sql.py
+++ AsunaRobot/modules/sql/approve_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText, Integer, func, distinct
 
-from YoneRobot.modules.sql import BASE, SESSION
+from AsunaRobot.modules.sql import BASE, SESSION
 
 
 class Approvals(BASE):

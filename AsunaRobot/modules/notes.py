92a0da4c7fb6359f4b62e496d4d4f63812d848dd
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-04-23 12:29:45 +0530

    Update notes.py

--- AsunaRobot/modules/notes.py
+++ AsunaRobot/modules/notes.py
@@ -3,14 +3,14 @@ from io import BytesIO
 import random
 from typing import Optional
 
-import YoneRobot.modules.sql.notes_sql as sql
-from YoneRobot import LOGGER, JOIN_LOGGER, SUPPORT_CHAT, dispatcher, DRAGONS
-from YoneRobot.modules.disable import DisableAbleCommandHandler
-from YoneRobot.modules.helper_funcs.handlers import MessageHandlerChecker
-from YoneRobot.modules.helper_funcs.chat_status import user_admin, connection_status
-from YoneRobot.modules.helper_funcs.misc import build_keyboard, revert_buttons
-from YoneRobot.modules.helper_funcs.msg_types import get_note_type
-from YoneRobot.modules.helper_funcs.string_handling import (
+import AsunaRobot.modules.sql.notes_sql as sql
+from AsunaRobot import LOGGER, JOIN_LOGGER, SUPPORT_CHAT, dispatcher, DRAGONS
+from AsunaRobot.modules.disable import DisableAbleCommandHandler
+from AsunaRobot.modules.helper_funcs.handlers import MessageHandlerChecker
+from AsunaRobot.modules.helper_funcs.chat_status import user_admin, connection_status
+from AsunaRobot.modules.helper_funcs.misc import build_keyboard, revert_buttons
+from AsunaRobot.modules.helper_funcs.msg_types import get_note_type
+from AsunaRobot.modules.helper_funcs.string_handling import (
     escape_invalid_curly_brackets,
 )
 from telegram import (

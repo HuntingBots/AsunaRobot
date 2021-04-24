5e5be82321e939016c6cc57953ed3bf114745ad4
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-04-24 16:53:26 +0530

    Update userinfo.py

--- AsunaRobot/modules/userinfo.py
+++ AsunaRobot/modules/userinfo.py
@@ -13,7 +13,7 @@ from telegram.ext.dispatcher import run_async
 from telegram.error import BadRequest
 from telegram.utils.helpers import escape_markdown, mention_html
 
-from YoneRobot import (
+from AsunaRobot import (
     DEV_USERS,
     OWNER_ID,
     DRAGONS,

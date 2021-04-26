28c563cb6ef9b8bcc6c626c2a0d23f0ac083cc8b
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-04-27 00:41:16 +0530

    Update __main__.py

--- AsunaRobot/__main__.py
+++ AsunaRobot/__main__.py
@@ -380,7 +380,7 @@ def asuna_about_callback(update, context):
                  \n❍ I have a note keeping system, blacklists, and even predetermined replies on certain keywords.
                  \n❍ I check for admins' permissions before executing any command and more stuffs
                  \n\n_yone's licensed under the GNU General Public License v3.0_
-                 \nHere is the [💾Repository](https://github.com/HuntingBots/AsunaRobot) 
+                 \nHere is the [💾Repository](https://github.com/) 
                  \n\nIf you have any question about AsunaRobot, let us know at .""",
             parse_mode=ParseMode.MARKDOWN,
             disable_web_page_preview=True,

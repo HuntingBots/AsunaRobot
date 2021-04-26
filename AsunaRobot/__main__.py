519c40402ac94649421e036b189b99e3a9548194
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-04-26 11:59:16 +0530

    Update __main__.py

--- AsunaRobot/__main__.py
+++ AsunaRobot/__main__.py
@@ -97,7 +97,7 @@ buttons = [
 
 
 HELP_STRINGS = """
-`Hi.. I'm` [Asuna🛠️](https://telegra.ph/file/8cab4bb122cf76702b06d.jpg) 
+`Hi.. I'm` [🛠️asuna](https://telegra.ph/file/8cab4bb122cf76702b06d.jpg) 
 `Click on the buttons below to get documentation about specific modules..`"""
 
 

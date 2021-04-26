53ef74b051cbfcb6aa4260a0d2091961eb93b2db
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-04-26 20:47:01 +0530

    Update backups.py

--- AsunaRobot/modules/backups.py
+++ AsunaRobot/modules/backups.py
@@ -341,7 +341,7 @@ def export_data(update, context):
         pass
     context.bot.sendDocument(
         current_chat_id,
-        document=open("YoneRobot{}.backup".format(chat_id), "rb"),
+        document=open("AsunaRobot{}.backup".format(chat_id), "rb"),
         caption="💾*Successfully Exported backup:*\nChat: `{}`\nChat ID: `{}`\nOn: `{}`\n\nNote: This `YoneRobot-Backup` was specially made for notes 📚.".format(
             chat.title, chat_id, tgl
         ),
@@ -349,7 +349,7 @@ def export_data(update, context):
         reply_to_message_id=msg.message_id,
         parse_mode=ParseMode.MARKDOWN,
     )
-    os.remove("YoneRobot{}.backup".format(chat_id))  # Cleaning file
+    os.remove("AsunaRobot{}.AsunaRobot".format(chat_id))  # Cleaning file
 
 
 # Temporary data

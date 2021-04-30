c944de63cf46b7ac672ead3ef96c17b3d4db60f5
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-04-30 15:12:36 +0530

    Update _pyrosaavn.py

--- AsunaRobot/modules/_pyrosaavn.py
+++ AsunaRobot/modules/_pyrosaavn.py
@@ -1,79 +1,36 @@
 # Thanks to @infinity_bots - Williambutcherbot
-# Jiosaavn Music
+import os
+
+import requests
+import wget
+from pyrogram import filters
+
+from AsunaRobot import pbot as Jebot
+from AsunaRobot.pyrogramee.dark import get_arg
 
 
-@app.on_message(filters.command("saavn") & ~filters.edited)
-@capture_err
-async def jssong(_, message):
-    global is_downloading
-    if len(message.command) < 2:
-        await message.reply_text("/saavn requires an argument.")
-        return
-    if is_downloading:
-        await message.reply_text("Another download is in progress, try again after sometime.")
-        return
-    is_downloading = True
-    text = message.text.split(None, 1)[1]
-    query = text.replace(" ", "%20")
-    m = await message.reply_text("Searching...")
+@Jebot.on_message(filters.command("saavn"))
+async def song(client, message):
+    message.chat.id
+    message.from_user["id"]
+    args = get_arg(message) + " " + "song"
+    if args.startswith(" "):
+        await message.reply("<b>Enter song name❗</b>")
+        return ""
+    m = await message.reply_text(
+        "Downloading your song,\nPlz wait ⏳️"
+    )
     try:
-        songs = await arq.saavn(query)
-        sname = songs[0].song
-        slink = songs[0].media_url
-        ssingers = songs[0].singers
-        await m.edit("Downloading")
-        song = await download_song(slink)
-        await m.edit("Uploading")
-        await message.reply_audio(
-            audio=song,
-            title=sname,
-            caption=f"「 `{format_size(await file_size_from_url(slink))}` 」",
-            performer=ssingers,
-            duration=int(songs[0].duration)
-        )
-        os.remove(song)
-        await m.delete()
+        r = requests.get(f"https://snobybuddymusic.herokuapp.com/result/?query={args}")
     except Exception as e:
-        is_downloading = False
         await m.edit(str(e))
         return
-    is_downloading = False
-
-# Deezer Music
-
-
-@app.on_message(filters.command("deezer") & ~filters.edited)
-@capture_err
-async def deezsong(_, message):
-    global is_downloading
-    if len(message.command) < 2:
-        await message.reply_text("/deezer requires an argument.")
-        return
-    if is_downloading:
-        await message.reply_text("Another download is in progress, try again after sometime.")
-        return
-    is_downloading = True
-    text = message.text.split(None, 1)[1]
-    query = text.replace(" ", "%20")
-    m = await message.reply_text("Searching...")
-    try:
-        songs = await arq.deezer(query, 1)
-        title = songs[0].title
-        url = songs[0].url
-        artist = songs[0].artist
-        await m.edit("Downloading")
-        song = await download_song(url)
-        await m.edit("Uploading")
-        await message.reply_audio(
-            audio=song,
-            title=title,
-            performer=artist,
-            duration=songs[0].duration,
-            caption=f"「 `{format_size(await file_size_from_url(url))}` 」")
-        os.remove(song)
-        await m.delete()
-    except Exception as e:
-        is_downloading = False
-        await m.edit(str(e))
-        return
-    is_downloading = False
+    sname = r.json()[0]["song"]
+    slink = r.json()[0]["media_url"]
+    ssingers = r.json()[0]["singers"]
+    file = wget.download(slink)
+    ffile = file.replace("mp4", "m4a")
+    os.rename(file, ffile)
+    await message.reply_audio(audio=ffile, title=sname, performer=ssingers)
+    os.remove(ffile)
+    await m.delete()

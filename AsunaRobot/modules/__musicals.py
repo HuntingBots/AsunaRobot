439d78fe82b21469c582c51902515348b9ac727c
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-05-02 00:25:17 +0530

    Update __musicals.py

--- AsunaRobot/modules/__musicals.py
+++ AsunaRobot/modules/__musicals.py
@@ -6,4 +6,5 @@ __help__ = """
 /music <i>query</i>: download song from yt servers. (API BASED)
 /lyrics <i>song name</i> : This plugin searches for song lyrics with song name.
 /glyrics <i> song name </i> : This plugin searches for song lyrics with song name and artist.
+/saavn` `<song name>`*:* Download song from saavn.
 """

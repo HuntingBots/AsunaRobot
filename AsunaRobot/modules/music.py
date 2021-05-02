eeb603b88eb153a2be91ea27aeaf6b360069ec57
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-05-02 08:12:44 +0530

    Update music.py

--- AsunaRobot/modules/music.py
+++ AsunaRobot/modules/music.py
@@ -16,23 +16,95 @@ from AsunaRobot.conf import get_str_key
 from AsunaRobot.pyrogramee.pluginshelper import get_text, progress
 from AsunaRobot import pbot
 
-# Thanks to @infinity_bots - Williambutcherbot
-import os
 
-import requests
-import wget
-from pyrogram import filters
+__MODULE__ = "Music"
+__HELP__ = """/ytmusic [link] To Download Music From Various Websites Including Youtube. [SUDOERS]
+/saavn [query] To Download Music From Saavn.
+/deezer [query] To Download Music From Deezer.
+/lyrics [query] To Get Lyrics Of A Song."""
 
-from arqlink import ARQ_API_BASE_URL as ARQ_API
-from Python_ARQ import ARQ
-from AsunaRobot import pbot as app
-from AsunaRobot.pyrogramee.dark import get_arg
+is_downloading = False
 
 
-arq = ARQ(ARQ_API)
+def get_file_extension_from_url(url):
+    url_path = urlparse(url).path
+    basename = os.path.basename(url_path)
+    return basename.split(".")[-1]
+
+
+async def download_youtube_audio(url: str, m=0):
+    global is_downloading
+    with youtube_dl.YoutubeDL({'format': 'bestaudio', 'writethumbnail': True, }) as ydl:
+        info_dict = ydl.extract_info(url, download=False)
+        if int(float(info_dict['duration'])) > 600:
+            if m != 0:
+                await m.edit("This music cannot be downloaded as it's too long.")
+            is_downloading = False
+            return
+        ydl.process_info(info_dict)
+        audio_file = ydl.prepare_filename(info_dict)
+        basename = audio_file.rsplit(".", 1)[-2]
+        if info_dict['ext'] == 'webm':
+            audio_file_opus = basename + ".opus"
+            ffmpeg.input(audio_file).output(audio_file_opus,
+                                            codec="copy", loglevel='error').overwrite_output().run()
+            os.remove(audio_file)
+            audio_file = audio_file_opus
+        thumbnail_url = info_dict['thumbnail']
+        thumbnail_file = basename + "." + \
+            get_file_extension_from_url(thumbnail_url)
+        title = info_dict['title']
+        performer = info_dict['uploader']
+        duration = int(float(info_dict['duration']))
+    return [title, performer, duration, audio_file, thumbnail_file]
+
+
+@app.on_message(filters.command("ytmusic") & ~filters.edited & filters.user(SUDOERS))
+@capture_err
+async def music(_, message):
+    global is_downloading
+    if len(message.command) != 2:
+        await message.reply_text("/ytmusic needs a link as argument")
+        return
+    url = message.text.split(None, 1)[1]
+    if is_downloading:
+        await message.reply_text("Another download is in progress, try again after sometime.")
+        return
+    is_downloading = True
+    m = await message.reply_text(f"Downloading {url}",
+                                 disable_web_page_preview=True)
+    try:
+        title, performer, duration, audio_file, thumbnail_file = await download_youtube_audio(url, message)
+    except Exception as e:
+        is_downloading = False
+        await m.edit(str(e))
+        return
+    await message.reply_audio(
+        audio_file, duration=duration,
+        performer=performer, title=title, thumb=thumbnail_file
+    )
+    await m.delete()
+    os.remove(audio_file)
+    os.remove(thumbnail_file)
+    is_downloading = False
+
+
+# Funtion To Download Song
+async def download_song(url):
+    song_name = f"{randint(6969, 6999)}.mp3"
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as resp:
+            if resp.status == 200:
+                f = await aiofiles.open(song_name, mode='wb')
+                await f.write(await resp.read())
+                await f.close()
+    return song_name
+
+# Jiosaavn Music
 
 
 @app.on_message(filters.command("saavn") & ~filters.edited)
+@capture_err
 async def jssong(_, message):
     global is_downloading
     if len(message.command) < 2:
@@ -68,111 +140,59 @@ async def jssong(_, message):
         return
     is_downloading = False
 
-
-GENIUS = get_str_key("GENIUS_API_TOKEN", None)
-
-
-# Lel, Didn't Get Time To Make New One So Used Plugin Made br @mrconfused and @sandy1709 dont edit credits
+# Deezer Music
 
 
-@pbot.on_message(filters.command(["lyric", "lyrics"]))
-async def _(client, message):
-    lel = await message.reply("Searching For Lyrics.....")
-    query = message.text
-    if not query:
-        await lel.edit("`What I am Supposed to find `")
+@app.on_message(filters.command("deezer") & ~filters.edited)
+@capture_err
+async def deezsong(_, message):
+    global is_downloading
+    if len(message.command) < 2:
+        await message.reply_text("/deezer requires an argument.")
         return
-
-    song = ""
-    song = Song.find_song(query)
-    if song:
-        if song.lyrics:
-            reply = song.format()
-        else:
-            reply = "Couldn't find any lyrics for that song! try with artist name along with song if still doesnt work try `.glyrics`"
-    else:
-        reply = "lyrics not found! try with artist name along with song if still doesnt work try `.glyrics`"
-
-    if len(reply) > 4095:
-        with io.BytesIO(str.encode(reply)) as out_file:
-            out_file.name = "lyrics.text"
-            await client.send_document(
-                message.chat.id,
-                out_file,
-                force_document=True,
-                allow_cache=False,
-                caption=query,
-                reply_to_msg_id=message.message_id,
-            )
-            await lel.delete()
-    else:
-        await lel.edit(reply)  # edit or reply
-
-
-@pbot.on_message(filters.command(["glyric", "glyrics"]))
-async def lyrics(client, message):
-
-    if r"-" in message.text:
-        pass
-    else:
-        await message.reply(
-            "`Error: please use '-' as divider for <artist> and <song>`\n"
-            "eg: `.glyrics Nicki Minaj - Super Bass`"
-        )
+    if is_downloading:
+        await message.reply_text("Another download is in progress, try again after sometime.")
         return
-
-    if GENIUS is None:
-        await message.reply(
-            "`Provide genius access token to config.py or Heroku Config first kthxbye!`"
-        )
-    else:
-        genius = lyricsgenius.Genius(GENIUS)
-        try:
-            args = message.text.split(".lyrics")[1].split("-")
-            artist = args[0].strip(" ")
-            song = args[1].strip(" ")
-        except Exception:
-            await message.reply("`Lel please provide artist and song names`")
-            return
-
-    if len(args) < 1:
-        await message.reply("`Please provide artist and song names`")
-        return
-
-    lel = await message.reply(f"`Searching lyrics for {artist} - {song}...`")
-
+    is_downloading = True
+    text = message.text.split(None, 1)[1]
+    query = text.replace(" ", "%20")
+    m = await message.reply_text("Searching...")
     try:
-        songs = genius.search_song(song, artist)
-    except TypeError:
-        songs = None
-
-    if songs is None:
-        await lel.edit(f"Song **{artist} - {song}** not found!")
+        songs = await arq.deezer(query, 1)
+        title = songs[0].title
+        url = songs[0].url
+        artist = songs[0].artist
+        await m.edit("Downloading")
+        song = await download_song(url)
+        await m.edit("Uploading")
+        await message.reply_audio(
+            audio=song,
+            title=title,
+            performer=artist,
+            duration=songs[0].duration,
+            caption=f"「 `{format_size(await file_size_from_url(url))}` 」")
+        os.remove(song)
+        await m.delete()
+    except Exception as e:
+        is_downloading = False
+        await m.edit(str(e))
         return
-    if len(songs.lyrics) > 4096:
-        await lel.edit("`Lyrics is too big, view the file to see it.`")
-        with open("lyrics.txt", "w+") as f:
-            f.write(f"Search query: \n{artist} - {song}\n\n{songs.lyrics}")
-        await client.send_document(
-            message.chat.id,
-            "lyrics.txt",
-            reply_to_msg_id=message.message_id,
-        )
-        os.remove("lyrics.txt")
-    else:
-        await lel.edit(
-            f"**Search query**: \n`{artist} - {song}`\n\n```{songs.lyrics}```"
-        )
-    return
+    is_downloading = False
+
+# Lyrics
 
 
-__mod_name__ = "Music"
-
-__help__ = """
-/video <i>query</i>: download video from youtube. 
-/deezer <i>query</i>: download from deezer. 
-/saavn <I>query</i>: download song from saavn. 
-/music <i>query</i>: download song from yt servers. (API BASED) 
-/lyrics <i>song name</i> : This plugin searches for song lyrics with song name.
-/glyrics <i> song name </i> : This plugin searches for song lyrics with song name and artist.
-"""
+@app.on_message(filters.command("lyrics"))
+async def lyrics_func(_, message):
+    if len(message.command) < 2:
+        await message.reply_text("**Usage:**\n/lyrics [QUERY]")
+        return
+    m = await message.reply_text("**Searching**")
+    query = message.text.strip().split(None, 1)[1]
+    song = await arq.lyrics(query)
+    lyrics = song.lyrics
+    if len(lyrics) < 4095:
+        await m.edit(f"__{lyrics}__")
+        return
+    lyrics = await paste(lyrics)
+    await m.edit(f"**LYRICS_TOO_LONG:** [URL]({lyrics})")

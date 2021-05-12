c0edf637e451832c1402d26e92474162f8cc8db9
RedAura <79897294+Red-Aura@users.noreply.github.com>
2021-05-12 10:09:07 +0530

    Update VoiceChatbot.py

--- AsunaRobot/modules/VoiceChatbot.py
+++ AsunaRobot/modules/VoiceChatbot.py
@@ -5,7 +5,7 @@ import aiofiles
 import aiohttp
 from random import randint
 from pyrogram import filters
-from lycia import LYCIA
+from AsunaRobot import pbot as LYCIA
 
 async def fetch(url):
     async with aiohttp.ClientSession() as session:

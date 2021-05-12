1439dab2bbad4a9a8d0610c17fff620221d46975
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-05-12 13:56:19 +0530

    Update __musicals.py

--- AsunaRobot/modules/__musicals.py
+++ AsunaRobot/modules/__musicals.py
@@ -1,6 +1,7 @@
 __mod_name__ = "◎Music"
 
 __help__ = """
+/saavn <i>query</i>: download song from Saavn servers.
 /deezer <i>query</i>: download from deezer.
 /video <i>query</i>: download video from youtube
 /song <i>query</i>: download from youtube

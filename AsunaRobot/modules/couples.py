12b2a54983862d93899b0ca743abbd91beb10792
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-05-13 22:00:46 +0530

    Update couples.py

--- AsunaRobot/modules/couples.py
+++ AsunaRobot/modules/couples.py
@@ -80,6 +80,8 @@ __New couple of the day may be chosen at 12AM {tomorrow}__"""
 
 
 __help__ = """
- ❍ /Couples - To Choose Couple Of The Day
+ ❍ /couples - To Choose Couple Of The Day
+
  """
-__mod_name__ = "couples"
+__mod_name__ = "Couples"
+

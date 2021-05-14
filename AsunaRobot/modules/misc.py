95d45adc975ed01bb20ce2ea16b7e657bee859a4
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-05-14 22:50:19 +0530

    Create misc.py

--- AsunaRobot/modules/misc.py
+++ AsunaRobot/modules/misc.py
@@ -86,35 +86,6 @@ __help__ = """
 *Markdown:*
  ❍ /markdownhelp*:* quick summary of how markdown works in telegram - can only be called in private chats
 *Paste:*
-• `/pat`*:* pats a user by a reply to the message.
- • `/neko`*:* sends random neko image
- • `/hug`*:* sends random hug gif
- • `/tickle`*:* sends random tickle gif
- • `/feed`*:* sends random feed gif
- • `/poke`*:* sends random poke gif
- • `/waifu`*:* sends random waifu sticker/image
- • `/kiss`*:* sends random kiss gif
- • `/baka`*:* sends random baka gif
- • `/smug`*:* sends random smug gif
- • `/foxgirl`*:* sends random foxgirl image
- • `/feed`*:* sends random feed gif
- • `/wallpaper`*:* sends random best anime wallpapers.
- 
-
-*Style your text with AsunaRobot!*
- • `/weebify <text>`*:* weebify your text!
- • `/bubble <text>`*:* bubble your text!
- • `/fbubble <text>`*:* bubble-filled your text!
- • `/square <text>`*:* square your text!
- • `/fsquare <text>`*:* square-filled your text!
- • `/blue <text>`*:* bluify your text!
- • `/latin <text>`*:* latinify your text!
- • `/lined <text>`*:* lined your text!
- • `/plet <text> `*:* text get funny emojify.
- 
- *Truth And Dare*
- • `/Truth`*:* for random truth.
- • `/dare`*:* for random dare.
  ❍ /paste*:* Saves replied content to `nekobin.com` and replies with a url
 *React:*
  ❍ /react*:* Reacts with a random reaction 

7c5d7085c3f8866c80cd5d3c4eb4aa66c7ce9f29
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-05-14 23:09:36 +0530

    Update misc.py

--- AsunaRobot/modules/helper_funcs/misc.py
+++ AsunaRobot/modules/helper_funcs/misc.py
@@ -72,7 +72,7 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
         pairs.append((modules[-1],))
 
     else:
-        pairs += [[EqInlineKeyboardButton("Go Back", callback_data="asuna_back"), EqInlineKeyboardButton("📋 Repository" url="https://github.com/HuntingBots/AsunaRobot") ]]
+        pairs += [[EqInlineKeyboardButton("Go Back", callback_data="asuna_back"), EqInlineKeyboardButton("💾Repository" url="https://github.com/HuntingBots/AsunaRobot") ]]
 
     return pairs
 

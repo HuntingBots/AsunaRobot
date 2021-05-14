9726f3a30bacd7918997bb56c3cd51aa03886f62
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-05-14 19:34:42 +0530

    Update conf.py

--- AsunaRobot/conf.py
+++ AsunaRobot/conf.py
@@ -19,6 +19,20 @@ def get_str_key(name, required=False):
     else:
         return data
 
+def get_str_key(name, required=False):
+    if name in DEFAULTS:
+        default = DEFAULTS[name]
+    else:
+        default = None
+    if not (data := env.str(name, default=default)) and not required:
+        log.warn("No str key: " + name)
+        return None
+    elif not data:
+        log.critical("No str key: " + name)
+        sys.exit(2)
+    else:
+        return data
+
 def get_int_key(name, required=False):
     if name in DEFAULTS:
         default = DEFAULTS[name]

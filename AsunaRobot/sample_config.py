3472f85d15012dd8fbfdbeb3f10d08565dc81109
Zyruz-ramu <77160871+Zyruz-ramu@users.noreply.github.com>
2021-05-21 12:51:37 +0530

    Update sample_config.py

--- AsunaRobot/sample_config.py
+++ AsunaRobot/sample_config.py
@@ -4,7 +4,7 @@ import os
 
 
 def get_user_list(config, key):
-    with open("{}/AsunaRobot/{}".format(os.getcwd(), config), "r") as json_file:
+    with open("{}/laylaRobot/{}".format(os.getcwd(), config), "r") as json_file:
         return json.load(json_file)[key]
 
 
@@ -17,14 +17,14 @@ class Config(object):
     API_ID = 123456  # integer value, dont use ""
     API_HASH = "awoo"
     TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 792109647  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "Kittu"
-    SUPPORT_CHAT = "Kora_Support"  # Your own group for support, do not add the @
+    OWNER_ID = 1777644370  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "ramu"
+    SUPPORT_CHAT = "unitedbotsupport"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
-        -1001253661229
+        -1001290052398
     )  # Prints any new group the bot is added to, prints just the name and ID.
     EVENT_LOGS = (
-        -1001190806654
+        -1001290052398
     )  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
 
     # RECOMMENDED

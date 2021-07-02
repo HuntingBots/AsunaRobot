44fdcf12d193b7ff982ae50827ef92b78fd34d0e
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-07-02 15:53:14 +0530

    Add files via upload

--- AsunaRobot/modules/__musicals.py
+++ AsunaRobot/modules/__musicals.py
@@ -1,10 +1,9 @@
 __mod_name__ = "◎Music"
 
 __help__ = """
-/saavn <i>query</i>: download song from Saavn servers.
-/deezer <i>query</i>: download from deezer.
 /video <i>query</i>: download video from youtube
-/song <i>query</i>: download from youtube
+/deezer <i>query</i>: download from deezer
+/music <i>query</i>: download song from yt servers. (API BASED)
 /lyrics <i>song name</i> : This plugin searches for song lyrics with song name.
 /glyrics <i> song name </i> : This plugin searches for song lyrics with song name and artist.
 """
--- AsunaRobot/modules/music.py
+++ AsunaRobot/modules/music.py
@@ -156,6 +156,48 @@ async def ytmusic(client, message: Message):
         if files and os.path.exists(files):
             os.remove(files)
 
+
+@pbot.on_message(filters.command(["deezer", "dsong"]))
+async def deezer(client, message: Message):
+    pablo = await client.send_message(message.chat.id, "Searching the song")
+    sgname = get_text(message)
+    if not sgname:
+        await pablo.edit("Invalid Command Syntax, Please Check Help Menu To Know More!")
+        return
+    link = f"https://api.deezer.com/search?q={sgname}&limit=1"
+    dato = requests.get(url=link).json()
+    match = dato.get("data")
+    urlhp = match[0]
+    urlp = urlhp.get("link")
+    thums = urlhp["album"]["cover_big"]
+    thum_f = wget.download(thums)
+    polu = urlhp.get("artist")
+    replo = urlp[29:]
+    urlp = f"https://starkapi.herokuapp.com/deezer/{replo}"
+    datto = requests.get(url=urlp).json()
+    mus = datto.get("url")
+    sname = f"{urlhp.get('title')}.mp3"
+    doc = requests.get(mus)
+    await client.send_chat_action(message.chat.id, "upload_audio")
+    await pablo.edit("`Downloading Song From Deezer!`")
+    with open(sname, "wb") as f:
+        f.write(doc.content)
+    c_time = time.time()
+    await pablo.edit(f"`Downloaded {sname}! Now Uploading Song...`")
+    await client.send_audio(
+        message.chat.id,
+        audio=open(sname, "rb"),
+        duration=int(urlhp.get("duration")),
+        title=str(urlhp.get("title")),
+        performer=str(polu.get("name")),
+        thumb=thum_f,
+        progress=progress,
+        progress_args=(pablo, c_time, f"`Uploading {sname} Song From Deezer!`", sname),
+    )
+    await client.send_chat_action(message.chat.id, "cancel")
+    await pablo.delete()
+
+
 def time_to_seconds(time):
     stringt = str(time)
     return sum(int(x) * 60 ** i for i, x in enumerate(reversed(stringt.split(":"))))
@@ -253,4 +295,5 @@ async def lyrics(client, message):
             f"**Search query**: \n`{artist} - {song}`\n\n```{songs.lyrics}```"
         )
     return
+
 

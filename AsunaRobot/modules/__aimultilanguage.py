7dc1e920dca8aa39c36bd16f30245cf7c16a68af
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-07-02 16:01:20 +0530

    Update __aimultilanguage.py

--- AsunaRobot/modules/__aimultilanguage.py
+++ AsunaRobot/modules/__aimultilanguage.py
@@ -14,16 +14,16 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-#    Kang with the credits
-#    Special credits to @AmarnathCdj
-import re
+#import re
 
 import emoji
-import requests
 
 IBM_WATSON_CRED_URL = "https://api.us-south.speech-to-text.watson.cloud.ibm.com/instances/bd6b59ba-3134-4dd4-aff2-49a79641ea15"
 IBM_WATSON_CRED_PASSWORD = "UQ1MtTzZhEsMGK094klnfa-7y_4MCpJY1yhd52MXOo3Y"
 url = "https://acobot-brainshop-ai-v1.p.rapidapi.com/get"
+import re
+
+import aiohttp
 from google_trans_new import google_translator
 from pyrogram import filters
 
@@ -33,52 +33,37 @@ from AsunaRobot.pyrogramee.pluginshelper import admins_only, edit_or_reply
 from AsunaRobot import pbot as asuna
 
 translator = google_translator()
+import requests
 
 
 def extract_emojis(s):
     return "".join(c for c in s if c in emoji.UNICODE_EMOJI)
 
 
+async def fetch(url):
+    try:
+        async with aiohttp.Timeout(10.0):
+            async with aiohttp.ClientSession() as session:
+                async with session.get(url) as resp:
+                    try:
+                        data = await resp.json()
+                    except:
+                        data = await resp.text()
+            return data
+    except:
+        print("AI response Timeout")
+        return
+
+
 asuna_chats = []
 en_chats = []
-# AI Chat (C) 2020-2021 by @InukaAsith
-"""
+
 @asuna.on_message(
-    filters.voice & filters.reply & ~filters.bot & ~filters.via_bot & ~filters.forwarded,
-    group=2,
+    filters.command("chatbot") & ~filters.edited & ~filters.bot & ~filters.private
 )
-async def hmm(client, message):
-    if not get_session(int(message.chat.id)):
-        message.continue_propagation()
-    if message.reply_to_message.from_user.id != BOT_ID:
-        message.continue_propagation()
-    previous_message = message
-    required_file_name = message.download()
-    if IBM_WATSON_CRED_URL is None or IBM_WATSON_CRED_PASSWORD is None:
-        await message.reply(
-            "You need to set the required ENV variables for this module. \nModule stopping"
-        )
-    else:
-        headers = {
-            "Content-Type": previous_message.voice.mime_type,
-        }
-        data = open(required_file_name, "rb").read()
-        response = requests.post(
-            IBM_WATSON_CRED_URL + "/v1/recognize",
-            headers=headers,
-            data=data,
-            auth=("apikey", IBM_WATSON_CRED_PASSWORD),
-        )
-        r = response.json()
-        print(r)
-        await client.send_message(message, r)
-"""
-
-
-@asuna.on_message(filters.command("chatbot") & ~filters.edited & ~filters.bot)
 @admins_only
 async def hmm(_, message):
-    global daisy_chats
+    global asuna_chats
     if len(message.command) != 2:
         await message.reply_text(
             "I only recognize `/chatbot on` and /chatbot `off only`"
@@ -90,20 +75,20 @@ async def hmm(_, message):
         lel = await edit_or_reply(message, "`Processing...`")
         lol = add_chat(int(message.chat.id))
         if not lol:
-            await lel.edit("Asuna AI Already Activated In This Chat")
+            await lel.edit("asuna AI Already Activated In This Chat")
             return
         await lel.edit(
-            f"Asuna AI Successfully Added For Users In The Chat {message.chat.id}"
+            f"asuna AI Successfully Added For Users In The Chat {message.chat.id}"
         )
 
     elif status == "OFF" or status == "off" or status == "Off":
         lel = await edit_or_reply(message, "`Processing...`")
         Escobar = remove_chat(int(message.chat.id))
         if not Escobar:
-            await lel.edit("Asuna AI Was Not Activated In This Chat")
+            await lel.edit("asuna AI Was Not Activated In This Chat")
             return
         await lel.edit(
-            f"Asuna AI Successfully Deactivated For Users In The Chat {message.chat.id}"
+            f"asuna AI Successfully Deactivated For Users In The Chat {message.chat.id}"
         )
 
     elif status == "EN" or status == "en" or status == "english":
@@ -120,14 +105,25 @@ async def hmm(_, message):
 
 
 @asuna.on_message(
-    filters.text & filters.reply & ~filters.bot & ~filters.via_bot & ~filters.forwarded,
+    filters.text
+    & filters.reply
+    & ~filters.bot
+    & ~filters.edited
+    & ~filters.via_bot
+    & ~filters.forwarded,
     group=2,
 )
 async def hmm(client, message):
     if not get_session(int(message.chat.id)):
-        message.continue_propagation()
-    if message.reply_to_message.from_user.id != BOT_ID:
-        message.continue_propagation()
+        return
+    if not message.reply_to_message:
+        return
+    try:
+        senderr = message.reply_to_message.from_user.id
+    except:
+        return
+    if senderr != BOT_ID:
+        return
     msg = message.text
     chat_id = message.chat.id
     if msg.startswith("/") or msg.startswith("@"):
@@ -135,30 +131,26 @@ async def hmm(client, message):
     if chat_id in en_chats:
         test = msg
         test = test.replace("asuna", "Aco")
-        test = test.replace("Asuna", "Aco")
-        querystring = {
-            "bid": "178",
-            "key": "sX5A2PcYZbsN5EY6",
-            "uid": "mashape",
-            "msg": {test},
-        }
-        headers = {
-            "x-rapidapi-key": "cf9e67ea99mshecc7e1ddb8e93d1p1b9e04jsn3f1bb9103c3f",
-            "x-rapidapi-host": "acobot-brainshop-ai-v1.p.rapidapi.com",
-        }
-        response = requests.request("GET", url, headers=headers, params=querystring)
-        result = response.text
-        result = result.replace('{"cnt":"', "")
-        result = result.replace('"}', "")
-        result = result.replace("Aco", "Asuna")
-        result = result.replace("<a href=\\", "<a href =")
-        result = result.replace("<\/a>", "</a>")
-        pro = result
+        test = test.replace("asuna", "Aco")
+        URL = "https://api.affiliateplus.xyz/api/chatbot?message=hi&botname=@AsunaRobot&ownername=@The_Ghost_Hunter"
+
+        try:
+            r = requests.request("GET", url=URL)
+        except:
+            return
+
+        try:
+            result = r.json()
+        except:
+            return
+
+        pro = result["message"]
         try:
             await asuna.send_chat_action(message.chat.id, "typing")
             await message.reply_text(pro)
-        except CFError as e:
-            print(e)
+        except CFError:
+            return
+
     else:
         u = msg.split()
         emj = extract_emojis(msg)
@@ -188,120 +180,46 @@ async def hmm(client, message):
         else:
             rm = msg
             # print (rm)
+        try:
             lan = translator.detect(rm)
+        except:
+            return
         test = rm
         if not "en" in lan and not lan == "":
-            test = translator.translate(test, lang_tgt="en")
-
+            try:
+                test = translator.translate(test, lang_tgt="en")
+            except:
+                return
         # test = emoji.demojize(test.strip())
 
-        # Kang with the credits bitches @InukaASiTH
+        # Kang with the credits bitches @InukaASiTHAsunaRobot
         test = test.replace("asuna", "Aco")
-        test = test.replace("Asuna", "Aco")
-        querystring = {
-            "bid": "178",
-            "key": "sX5A2PcYZbsN5EY6",
-            "uid": "mashape",
-            "msg": {test},
-        }
-        headers = {
-            "x-rapidapi-key": "cf9e67ea99mshecc7e1ddb8e93d1p1b9e04jsn3f1bb9103c3f",
-            "x-rapidapi-host": "acobot-brainshop-ai-v1.p.rapidapi.com",
-        }
-        response = requests.request("GET", url, headers=headers, params=querystring)
-        result = response.text
-        result = result.replace('{"cnt":"', "")
-        result = result.replace('"}', "")
-        result = result.replace("Aco", "Asuna")
-        result = result.replace("<a href=\\", "<a href =")
-        result = result.replace("<\/a>", "</a>")
-        pro = result
+        test = test.replace("asuna", "Aco")
+        URL = f"https://api.affiliateplus.xyz/api/chatbot?message={test}&botname=@AsunaRobot&ownername=@The_Ghost_Hunter"
+        try:
+            r = requests.request("GET", url=URL)
+        except:
+            return
+
+        try:
+            result = r.json()
+        except:
+            return
+        pro = result["message"]
         if not "en" in lan and not lan == "":
-            pro = translator.translate(pro, lang_tgt=lan[0])
+            try:
+                pro = translator.translate(pro, lang_tgt=lan[0])
+            except:
+                return
         try:
             await asuna.send_chat_action(message.chat.id, "typing")
             await message.reply_text(pro)
-        except CFError as e:
-            print(e)
-
-
-@asuna.on_message(filters.text & filters.private & filters.reply & ~filters.bot)
-async def inuka(client, message):
-    msg = message.text
-    if msg.startswith("/") or msg.startswith("@"):
-        message.continue_propagation()
-    u = msg.split()
-    emj = extract_emojis(msg)
-    msg = msg.replace(emj, "")
-    if (
-        [(k) for k in u if k.startswith("@")]
-        and [(k) for k in u if k.startswith("#")]
-        and [(k) for k in u if k.startswith("/")]
-        and re.findall(r"\[([^]]+)]\(\s*([^)]+)\s*\)", msg) != []
-    ):
-
-        h = " ".join(filter(lambda x: x[0] != "@", u))
-        km = re.sub(r"\[([^]]+)]\(\s*([^)]+)\s*\)", r"", h)
-        tm = km.split()
-        jm = " ".join(filter(lambda x: x[0] != "#", tm))
-        hm = jm.split()
-        rm = " ".join(filter(lambda x: x[0] != "/", hm))
-    elif [(k) for k in u if k.startswith("@")]:
-
-        rm = " ".join(filter(lambda x: x[0] != "@", u))
-    elif [(k) for k in u if k.startswith("#")]:
-        rm = " ".join(filter(lambda x: x[0] != "#", u))
-    elif [(k) for k in u if k.startswith("/")]:
-        rm = " ".join(filter(lambda x: x[0] != "/", u))
-    elif re.findall(r"\[([^]]+)]\(\s*([^)]+)\s*\)", msg) != []:
-        rm = re.sub(r"\[([^]]+)]\(\s*([^)]+)\s*\)", r"", msg)
-    else:
-        rm = msg
-        # print (rm)
-        lan = translator.detect(rm)
-    test = rm
-    if not "en" in lan and not lan == "":
-        test = translator.translate(test, lang_tgt="en")
-
-    # test = emoji.demojize(test.strip())
-
-    # Kang with the credits bitches @InukaASiTH
-    test = test.replace("asuna", "Aco")
-    test = test.replace("Asuna", "Aco")
-    querystring = {
-        "bid": "178",
-        "key": "sX5A2PcYZbsN5EY6",
-        "uid": "mashape",
-        "msg": {test},
-    }
-    headers = {
-        "x-rapidapi-key": "cf9e67ea99mshecc7e1ddb8e93d1p1b9e04jsn3f1bb9103c3f",
-        "x-rapidapi-host": "acobot-brainshop-ai-v1.p.rapidapi.com",
-    }
-    response = requests.request("GET", url, headers=headers, params=querystring)
-    result = response.text
-    result = result.replace('{"cnt":"', "")
-    result = result.replace('"}', "")
-    result = result.replace("Aco", "Asuna")
-    result = result.replace("<a href=\\", "<a href =")
-    result = result.replace("<\/a>", "</a>")
-    pro = result
-    if not "en" in lan and not lan == "":
-        pro = translator.translate(pro, lang_tgt=lan[0])
-    try:
-        await daisyx.send_chat_action(message.chat.id, "typing")
-        await message.reply_text(pro)
-    except CFError as e:
-        print(e)
+        except CFError:
+            return
 
 
 @asuna.on_message(
-    filters.regex("Asuna||Asuna|asuna|Asuna")
-    & ~filters.bot
-    & ~filters.via_bot
-    & ~filters.forwarded
-    & ~filters.reply
-    & ~filters.channel
+    filters.text & filters.private & ~filters.edited & filters.reply & ~filters.bot
 )
 async def inuka(client, message):
     msg = message.text
@@ -335,62 +253,129 @@ async def inuka(client, message):
     else:
         rm = msg
         # print (rm)
+    try:
         lan = translator.detect(rm)
+    except:
+        return
     test = rm
     if not "en" in lan and not lan == "":
-        test = translator.translate(test, lang_tgt="en")
+        try:
+            test = translator.translate(test, lang_tgt="en")
+        except:
+            return
 
-    # test = emoji.demojize(test.strip())
+    # test = emoji.demojize(test.strip()) 
 
     # Kang with the credits bitches @InukaASiTH
-    test = test.replace("daisy", "Aco")
-    test = test.replace("Daisy", "Aco")
-    querystring = {
-        "bid": "178",
-        "key": "sX5A2PcYZbsN5EY6",
-        "uid": "mashape",
-        "msg": {test},
-    }
-    headers = {
-        "x-rapidapi-key": "cf9e67ea99mshecc7e1ddb8e93d1p1b9e04jsn3f1bb9103c3f",
-        "x-rapidapi-host": "acobot-brainshop-ai-v1.p.rapidapi.com",
-    }
-    response = requests.request("GET", url, headers=headers, params=querystring)
-    result = response.text
-    result = result.replace('{"cnt":"', "")
-    result = result.replace('"}', "")
-    result = result.replace("Aco", "Daisy")
-    result = result.replace("<a href=\\", "<a href =")
-    result = result.replace("<\/a>", "</a>")
-    pro = result
+    test = test.replace("asuna", "Aco")
+    test = test.replace("asuna", "Aco")
+    URL = f"https://api.affiliateplus.xyz/api/chatbot?message={test}&botname=@AsunaRobot&ownername=@The_Ghost_Hunter"
+    try:
+        r = requests.request("GET", url=URL)
+    except:
+        return
+
+    try:
+        result = r.json()
+    except:
+        return
+
+    pro = result["message"]
     if not "en" in lan and not lan == "":
         pro = translator.translate(pro, lang_tgt=lan[0])
     try:
         await asuna.send_chat_action(message.chat.id, "typing")
         await message.reply_text(pro)
-    except CFError as e:
-        print(e)
+    except CFError:
+        return
+
+
+@asuna.on_message(
+    filters.regex("asuna|Asuna|ASUNA")
+    & ~filters.bot
+    & ~filters.via_bot
+    & ~filters.forwarded
+    & ~filters.reply
+    & ~filters.channel
+    & ~filters.edited
+)
+async def inuka(client, message):
+    msg = message.text
+    if msg.startswith("/") or msg.startswith("@"):
+        message.continue_propagation()
+    u = msg.split()
+    emj = extract_emojis(msg)
+    msg = msg.replace(emj, "")
+    if (
+        [(k) for k in u if k.startswith("@")]
+        and [(k) for k in u if k.startswith("#")]
+        and [(k) for k in u if k.startswith("/")]
+        and re.findall(r"\[([^]]+)]\(\s*([^)]+)\s*\)", msg) != []
+    ):
+
+        h = " ".join(filter(lambda x: x[0] != "@", u))
+        km = re.sub(r"\[([^]]+)]\(\s*([^)]+)\s*\)", r"", h)
+        tm = km.split()
+        jm = " ".join(filter(lambda x: x[0] != "#", tm))
+        hm = jm.split()
+        rm = " ".join(filter(lambda x: x[0] != "/", hm))
+    elif [(k) for k in u if k.startswith("@")]:
+
+        rm = " ".join(filter(lambda x: x[0] != "@", u))
+    elif [(k) for k in u if k.startswith("#")]:
+        rm = " ".join(filter(lambda x: x[0] != "#", u))
+    elif [(k) for k in u if k.startswith("/")]:
+        rm = " ".join(filter(lambda x: x[0] != "/", u))
+    elif re.findall(r"\[([^]]+)]\(\s*([^)]+)\s*\)", msg) != []:
+        rm = re.sub(r"\[([^]]+)]\(\s*([^)]+)\s*\)", r"", msg)
+    else:
+        rm = msg
+        # print (rm)
+    try:
+        lan = translator.detect(rm)
+    except:
+        return
+    test = rm
+    if not "en" in lan and not lan == "":
+        try:
+            test = translator.translate(test, lang_tgt="en")
+        except:
+            return
+
+    # test = emoji.demojize(test.strip())
+
+    # Kang with the credits bitches @InukaASiTH
+    test = test.replace("asuna", "Aco")
+    test = test.replace("asuna", "Aco")
+    URL = f"https://api.affiliateplus.xyz/api/chatbot?message={test}&botname=@AsunaRobot&ownername=@The_Ghost_Hunter"
+    try:
+        r = requests.request("GET", url=URL)
+    except:
+        return
+
+    try:
+        result = r.json()
+    except:
+        return
+    pro = result["message"]
+    if not "en" in lan and not lan == "":
+        try:
+            pro = translator.translate(pro, lang_tgt=lan[0])
+        except Exception:
+            return
+    try:
+        await asuna.send_chat_action(message.chat.id, "typing")
+        await message.reply_text(pro)
+    except CFError:
+        return
 
 
 __help__ = """
 <b> Chatbot </b>
-<i> PRESENTING ASUNA AI 3.0. THE ONLY AI SYSTEM WHICH CAN DETECT & REPLY UPTO 200 LANGUAGES </i>
- - /chatbot <i>ON/OFF</i>: Enables and disables AI Chat mode (EXCLUSIVE)
-* DaisyAI can detect and reply upto 200 languages by now *
+Asuna AI 3.0 IS THE ONLY AI SYSTEM WHICH CAN DETECT & REPLY UPTO 200 LANGUAGES
+ - /chatbot [ON/OFF]: Enables and disables AI Chat mode (EXCLUSIVE)
  - /chatbot EN : Enables English only chatbot
  
-<b> Lydia </b>
-<i> PRESENTING DAISY'S LYDIA, EXCLUSIVE CHAT FEATURE DETECT UPTO 200 LANGUAGES & REPLY USING LYDIA AI</i>
- - /addlydia: Activates lydia on your group
-* Daisy AI can detect and reply upto 200 languages by now *
- - /enlydia : Enables English only chat AI
- - /rmlydia : Deactivates lydia on your group (UNSTABLE)
- 
-<b> Assistant </b>
- - /ask <i>question</i>: Ask question from daisy
- - /ask <i> reply to voice note</i>: Get voice reply
- 
-<i> Lydia AI can be unstable sometimes </i>
 """
 
-__mod_name__ = "AI Assistant"
+__mod_name__ = "chatbot"

665e94a1d65989c1692ae0cfa64ff78a566a910a
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-07-07 08:45:50 +0530

    Update sample_config.py

--- AsunaRobot/sample_config.py
+++ AsunaRobot/sample_config.py
@@ -18,13 +18,13 @@ class Config(object):
     API_HASH = "awoo"
     TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
     OWNER_ID = 792109647  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "Kittu"
-    SUPPORT_CHAT = "Kora_Support"  # Your own group for support, do not add the @
+    OWNER_USERNAME = "The_Ghost_Hunter"
+    SUPPORT_CHAT = "AsunaRobotSupport"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
-        -1001253661229
+        -1001432609692
     )  # Prints any new group the bot is added to, prints just the name and ID.
     EVENT_LOGS = (
-        -1001190806654
+        -1001432768300
     )  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
 
     # RECOMMENDED

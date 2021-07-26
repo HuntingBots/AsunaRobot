53bee0495b52ee261974a84baf3cc141c68d9e6b
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-07-26 19:44:34 +0530

    Create pyrogram.py

---
+++ AsunaRobot/services/pyrogram.py
@@ -0,0 +1,15 @@
+import logging
+
+from pyrogram import Client
+#from pyromod import listen
+from AsunaRobot.conf import get_int_key, get_str_key
+
+TOKEN = get_str_key("TOKEN", required=True)
+APP_ID = get_int_key("APP_ID", required=True)
+APP_HASH = get_str_key("APP_HASH", required=True)
+session_name = TOKEN.split(":")[0]
+pbot = Client(session_name, api_id=APP_ID, api_hash=APP_HASH, bot_token=TOKEN)
+
+# disable logging for pyrogram [not for ERROR logging]
+logging.getLogger("pyrogram").setLevel(level=logging.ERROR)
+pbot.start()

18d8e5dc76b30b85d5b73007b80787166ed4a753
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-07-26 19:42:26 +0530

    Update paste.py

--- AsunaRobot/modules/paste.py
+++ AsunaRobot/modules/paste.py
@@ -1,37 +1,37 @@
+
+import os
+
 import requests
-from AsunaRobot import dispatcher
-from AsunaRobot.modules.disable import DisableAbleCommandHandler
-from telegram import ParseMode, Update
-from telegram.ext import CallbackContext
-from AsunaRobot import events as register
+from pyrogram import filters
+
+from AsunaRobot.function.pluginhelpers import edit_or_reply, get_text
+from AsunaRobot.services.pyrogram import pbot
 
 
-@register(command='paste', pass_args=True)
-def paste(update: Update, context: CallbackContext):
-    args = context.args
-    message = update.effective_message
-
-    if message.reply_to_message:
-        data = message.reply_to_message.text
-
-    elif len(args) >= 1:
-        data = message.text.split(None, 1)[1]
-
-    else:
-        message.reply_text("What am I supposed to do with this?")
-        return
-
+@pbot.on_message(filters.command("paste") & ~filters.edited & ~filters.bot)
+async def paste(client, message):
+    pablo = await edit_or_reply(message, "`Please Wait.....`")
+    tex_t = get_text(message)
+    message_s = tex_t
+    if not tex_t:
+        if not message.reply_to_message:
+            await pablo.edit("`Reply To File / Give Me Text To Paste!`")
+            return
+        if not message.reply_to_message.text:
+            file = await message.reply_to_message.download()
+            m_list = open(file, "r").read()
+            message_s = m_list
+            print(message_s)
+            os.remove(file)
+        elif message.reply_to_message.text:
+            message_s = message.reply_to_message.text
     key = (
-        requests.post("https://nekobin.com/api/documents", json={"content": data})
+        requests.post("https://nekobin.com/api/documents", json={"content": message_s})
         .json()
         .get("result")
         .get("key")
     )
-
     url = f"https://nekobin.com/{key}"
-
-    reply_text = f"Nekofied to *Nekobin* : {url}"
-
-    message.reply_text(
-        reply_text, parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True
-    )
+    raw = f"https://nekobin.com/raw/{key}"
+    reply_text = f"Pasted Text To [NekoBin]({url}) And For Raw [Click Here]({raw})"
+    await pablo.edit(reply_text)

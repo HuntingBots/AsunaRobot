4f3a14f0b067e7775b9f1f9405c05a4a26dd8719
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-07-26 20:38:11 +0530

    Update paste.py

--- AsunaRobot/modules/paste.py
+++ AsunaRobot/modules/paste.py
@@ -1,37 +1,66 @@
-
+import logging
 import os
+from datetime import datetime
 
 import requests
-from pyrogram import filters
+from requests import exceptions, get
+from telethon import events
+from telethon.errors.rpcerrorlist import YouBlockedUserError
 
-from AsunaRobot.pyrogramee.pluginshelper import edit_or_reply, get_text
-from AsunaRobot.services.pyrogram import pbot
+from AsunaRobot import telethn 
+from AsunaRobot.events import register as asuna 
 
+@asuna(pattern="^/paste ?(.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    datetime.now()
+    if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):
+        os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)
+    input_str = event.pattern_match.group(1)
+    message = "SYNTAX: `.neko <long text to include>`"
+    if input_str:
+        message = input_str
+    elif event.reply_to_msg_id:
+        previous_message = await event.get_reply_message()
+        if previous_message.media:
+            downloaded_file_name = await telethn.download_media(
+                previous_message,
+                Config.TMP_DOWNLOAD_DIRECTORY,
+                progress_callback=progress,
+            )
+            m_list = None
+            with open(downloaded_file_name, "rb") as fd:
+                m_list = fd.readlines()
+            message = ""
+            for m in m_list:
+                # message += m.decode("UTF-8") + "\r\n"
+                message += m.decode("UTF-8")
+            os.remove(downloaded_file_name)
+        else:
+            message = previous_message.message
+    else:
+        message = "SYNTAX: `.neko <long text to include>`"
+    if downloaded_file_name.endswith(".py"):
+        py_file = ""
+        py_file += ".py"
+        data = message
+        key = (
+            requests.post("https://nekobin.com/api/documents", json={"content": data})
+            .json()
+            .get("result")
+            .get("key")
+        )
+        url = f"https://nekobin.com/{key}{py_file}"
+    else:
+        data = message
+        key = (
+            requests.post("https://nekobin.com/api/documents", json={"content": data})
+            .json()
+            .get("result")
+            .get("key")
+        )
+        url = f"https://nekobin.com/{key}"
 
-@pbot.on_message(filters.command("paste") & ~filters.edited & ~filters.bot)
-async def paste(client, message):
-    pablo = await edit_or_reply(message, "`Please Wait.....`")
-    tex_t = get_text(message)
-    message_s = tex_t
-    if not tex_t:
-        if not message.reply_to_message:
-            await pablo.edit("`Reply To File / Give Me Text To Paste!`")
-            return
-        if not message.reply_to_message.text:
-            file = await message.reply_to_message.download()
-            m_list = open(file, "r").read()
-            message_s = m_list
-            print(message_s)
-            os.remove(file)
-        elif message.reply_to_message.text:
-            message_s = message.reply_to_message.text
-    key = (
-        requests.post("https://nekobin.com/api/documents", json={"content": data})
-        .json()
-        .get("result")
-        .get("key")
-    )
-    url = f"https://nekobin.com/{key}"
-    raw = f"https://nekobin.com/raw/{key}"
-    reply_text = f"Pasted Text To [NekoBin]({url}) And For Raw [Click Here]({raw})"
-    await pablo.edit(reply_text)
+    reply_text = f"Pasted to Nekobin : [neko]({url})"
+    await event.edit(reply_text)

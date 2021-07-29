ff68a2de6152ff2aedb2070ca83505f44c0de2f3
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-07-29 14:05:34 +0530

    Update paste.py

--- AsunaRobot/modules/paste.py
+++ AsunaRobot/modules/paste.py
@@ -1,43 +1,76 @@
+import httpx
 import requests
-from AsunaRobot import dispatcher
-from AsunaRobot.modules.disable import DisableAbleCommandHandler
+
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, run_async
 
+from AsunaRobot import pgram, http, dispatcher
+from AsunaRobot import dispatcher
+from AsunaRobot.modules.disable import DisableAbleCommandHandler
 
-@run_async
 def paste(update: Update, context: CallbackContext):
     args = context.args
     message = update.effective_message
 
     if message.reply_to_message:
-        data = message.reply_to_message.text
-
+        pasting = message.reply_to_message.text    
+    
     elif len(args) >= 1:
-        data = message.text.split(None, 1)[1]
+        pasting = message.text.split(None, 1)[1]
 
     else:
         message.reply_text("What am I supposed to do with this?")
         return
+   
+    TIMEOUT = 3
+    key = (
+        requests.post("https://nekobin.com/", data=pasting, timeout=TIMEOUT)
+        .json()
+        .get("result")
+        .get("key")
+    )
 
-    key = requests.post(
-        'https://nekobin.com/api/documents', json={
-            "content": data
-        }).json().get('result').get('key')
+    url = f"https://nekobin.com/{key}"
 
-    url = f'https://ezup.dev/p/{key}'
-
-    reply_text = f'Nekofied to *Nekobin* : {url}'
+    reply_text = f"Pasted to *Nekobin* : {url}"
 
     message.reply_text(
-        reply_text,
-        parse_mode=ParseMode.MARKDOWN,
-        disable_web_page_preview=True)
+        reply_text, parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True,
+    )
 
 
-PASTE_HANDLER = DisableAbleCommandHandler("paste", paste)
-dispatcher.add_handler(PASTE_HANDLER)
+def hastebin(update: Update, context: CallbackContext):
+    bot = context.bot
+    args = context.args
+    msg = update.effective_message  
+    
+    if msg.reply_to_message:        
+        mean = msg.reply_to_message.text
+               
+    elif len(args) >= 1:
+        mean = msg.text.split(None, 1)[1]
+   
+    else:
+    	msg.reply_text("reply to any message or just do /paste <what you want to paste>")  
+    	return
+                                                                              
+    url = "https://del.dog/documents"
+    key = (
+        requests.post(url, data=mean.encode("UTF-8"))
+        .json()       
+        .get('key')
+    )
+    pasted = f"Pasted to dogbin: https://del.dog/{r['key']}"
+    msg.reply_text(pasted, disable_web_page_preview=True)
+    
+    
+    
 
-__command_list__ = ["paste"]
-__handlers__ = [PASTE_HANDLER]
+NEKO_BIN_HANDLER = DisableAbleCommandHandler("dpaste", paste, run_async)
+DOG_BIN_HANDLER = DisableAbleCommandHandler("paste", dogbin, run_async)
 
+dispatcher.add_handler(NEKO_BIN_HANDLER)
+dispatcher.add_handler(DOG_BIN_HANDLER)
+
+__command_list__ = ["dpaste", "dogpaste"]
+__handlers__ = [DOG_BIN_HANDLER]

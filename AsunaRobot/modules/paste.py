ab8284264c79a051b8702c9bd0373ea1c8eed7e0
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-08-09 14:32:52 +0530

    Update paste.py

--- AsunaRobot/modules/paste.py
+++ AsunaRobot/modules/paste.py
@@ -1,43 +1,50 @@
-import requests
-from AsunaRobot import dispatcher
-from AsunaRobot.modules.disable import DisableAbleCommandHandler
-from telegram import ParseMode, Update
-from telegram.ext import CallbackContext, run_async
+# recoded by @The_Ghost_Hunter, @My_Asuna_Robot on Telegram
+
+import asyncio
+import os
+import re
+
+import aiofiles
+from pyrogram import filters
+from AsunaRobot.services.paste import paste 
+from AsunaRobot import pbot as app
+
+pattern = re.compile(r"^text/|json$|yaml$|xml$|toml$|x-sh$|x-shellscript$")
 
 
-@run_async
-def paste(update: Update, context: CallbackContext):
-    args = context.args
-    message = update.effective_message
+async def isPreviewUp(preview: str) -> bool:
+    for _ in range(7):
+        try:
+            async with head(preview, timeout=2) as resp:
+                status = resp.status
+                size = resp.content_length
+        except asyncio.exceptions.TimeoutError:
+            return False
+        if status == 404 or (status == 200 and size == 0):
+            await asyncio.sleep(0.4)
+        else:
+            return True if status == 200 else False
+    return False
 
+
+@app.on_message(filters.command("paste") & ~filters.edited)
+async def paste_func(_, message):
     if message.reply_to_message:
-        data = message.reply_to_message.text
+        m = await message.reply_text("Pasting...")
+        if message.reply_to_message.text:
+            content = str(message.reply_to_message.text)
 
-    elif len(args) >= 1:
-        data = message.text.split(None, 1)[1]
-
+        elif message.reply_to_message.document:
+            document = message.reply_to_message.document
+            if document.file_size > 1048576:
+                return await m.edit("You can only paste files smaller than 1MB.")
+            if not pattern.search(document.mime_type):
+                return await m.edit("Only text files can be pasted.")
+            doc = await message.reply_to_message.download()
+            async with aiofiles.open(doc, mode="r") as f:
+                content = await f.read()
+            os.remove(doc)
+        link = await paste(content)
+        await m.edit(link)
     else:
-        message.reply_text("What am I supposed to do with this?")
-        return
-
-    key = (
-        requests.post("https://nekobin.com/api/documents", json={"content": data})
-        .json()
-        .get("result")
-        .get("key")
-    )
-
-    url = f"https://nekobin.com/{key}"
-
-    reply_text = f"Nekofied to *Nekobin* : {url}"
-
-    message.reply_text(
-        reply_text, parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True
-    )
-
-
-PASTE_HANDLER = DisableAbleCommandHandler("paste", paste)
-dispatcher.add_handler(PASTE_HANDLER)
-
-__command_list__ = ["paste"]
-__handlers__ = [PASTE_HANDLER]
+        await message.reply_text("Reply To A Message With /paste")

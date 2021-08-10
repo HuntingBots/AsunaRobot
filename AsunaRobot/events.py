fb7fdfd476a56412a01510880b9f8b5be903571c
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-08-11 02:33:52 +0530

    Update events.py

--- AsunaRobot/events.py
+++ AsunaRobot/events.py
@@ -62,11 +62,3 @@ def callbackquery(**args):
         return func
 
     return decorator
-
-path = "AsunaRobot/modules/*.py"
-files = glob.glob(path)
-for name in files:
-    with open(name) as f:
-        path1 = Path(f.name)
-        shortname = path1.stem
-        load_module(shortname.replace(".py", ""))

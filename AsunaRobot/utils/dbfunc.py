19cac7355cbd9661e66f9684cb83fc57e9d3570b
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-08-11 03:03:27 +0530

    Update dbfunc.py

--- AsunaRobot/utils/dbfunc.py
+++ AsunaRobot/utils/dbfunc.py
@@ -1,10 +1,13 @@
 from AsunaRobot.mongo import db
 from typing import Dict, List, Union
 
+
 coupledb = db.couple
+karmadb = db.karma
 
 
 # Couple Chooser
+
 
 async def _get_lovers(chat_id: int):
     lovers = coupledb.find_one({"chat_id": chat_id})
@@ -26,18 +29,50 @@ async def get_couple(chat_id: int, date: str):
 async def save_couple(chat_id: int, date: str, couple: dict):
     lovers = await _get_lovers(chat_id)
     lovers[date] = couple
-    coupledb.update_one(
-        {"chat_id": chat_id},
-        {
-            "$set": {
-                "couple": lovers
-            }
-        },
-        upsert=True
-    )
+    coupledb.update_one({"chat_id": chat_id}, {"$set": {"couple": lovers}}, upsert=True)
 
 
-# Alpha integer 
+# Karma functions
+
+
+async def get_karmas_count() -> dict:
+    chats = karmadb.find({"chat_id": {"$lt": 0}})
+    if not chats:
+        return {}
+    chats_count = 0
+    karmas_count = 0
+    for chat in await chats.to_list(length=1000000):
+        for i in chat["karma"]:
+            karmas_count += chat["karma"][i]["karma"]
+        chats_count += 1
+    return {"chats_count": chats_count, "karmas_count": karmas_count}
+
+
+async def get_karmas(chat_id: int) -> Dict[str, int]:
+    karma = karmadb.find_one({"chat_id": chat_id})
+    if karma:
+        karma = karma["karma"]
+    else:
+        karma = {}
+    return karma
+
+
+async def get_karma(chat_id: int, name: str) -> Union[bool, dict]:
+    name = name.lower().strip()
+    karmas = await get_karmas(chat_id)
+    if name in karmas:
+        return karmas[name]
+
+
+async def update_karma(chat_id: int, name: str, karma: dict):
+    name = name.lower().strip()
+    karmas = await get_karmas(chat_id)
+    karmas[name] = karma
+    karmadb.update_one({"chat_id": chat_id}, {"$set": {"karma": karmas}}, upsert=True)
+
+
+# Alpha integer
+
 
 async def int_to_alpha(user_id: int) -> str:
     alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]

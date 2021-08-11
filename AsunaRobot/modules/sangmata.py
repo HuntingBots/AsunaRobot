039333f60f022a2939954affabf4a21781038ae7
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-08-11 10:40:32 +0530

    Create sangmata.py

---
+++ AsunaRobot/modules/sangmata.py
@@ -0,0 +1,53 @@
+# Copyright (C) 2021 AsunaRobot
+# made by @The_Ghost_Hunter on Telegram.
+# github account : https://github.com/HuntingBots/
+# This file is part of AsunaRobot (Telegram Bot)
+
+
+import datetime
+from telethon import events
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+from telethon.tl.functions.account import UpdateNotifySettingsRequest
+from AsunaRobot.events import register as Asuna
+
+@Asuna(pattern="^/sg ?(.*)")
+async def _(event):
+    if event.fwd_from:
+        return 
+    if not event.reply_to_msg_id:
+       await event.edit("```Reply to any user message.```")
+       return
+    reply_message = await event.get_reply_message() 
+    if not reply_message.text:
+       await event.edit("```reply to text message```")
+       return
+    chat = "@SangMataInfo_bot"
+    sender = reply_message.sender
+    if reply_message.sender.bot:
+       await event.edit("```Reply to actual users message.```")
+       return
+    await event.edit("```Processing```")
+    async with events.conversation(chat) as conv:
+          try:     
+              response = conv.wait_event(events.NewMessage(incoming=True,from_users=461843263))
+             # await events.forward_messages(chat, reply_message)
+              await silently_send_message(chat,"/generate")
+              response = await response 
+          except YouBlockedUserError: 
+              await event.reply("```Please unblock @sangmatainfo_bot and try again```")
+              return
+          if response.text.startswith("Forward"):
+             await event.edit("```can you kindly disable your forward privacy settings for good?```")
+          else: 
+             await event.edit(f"{response.message.message}")
+
+
+
+
+__mod_name__ = "Sangmata"
+
+__help__ = """
+ ❍ sangmata *:* /sg - View user name history.
+"""
+__button__ = ""
+__buttons__ = ""

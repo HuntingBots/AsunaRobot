3f6caa5a238eef7a3f5ab73c0bf623c701835a96
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-09-07 19:35:45 +0530

    Add files via upload

--- Procfile
+++ Procfile
@@ -1,3 +1,1 @@
 worker: python3 -m AsunaRobot
-web: python3 -m AsunaRobot
-ps:scale worker=1
--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 @echo off
-TITLE Asuna Robot 
-:: Enables virtual env mode and then starts asuna
+TITLE Asuna Robot
+:: Enables virtual env mode and then starts saitama
 env\scripts\activate.bat && py -m AsunaRobot
